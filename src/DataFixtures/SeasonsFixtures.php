2477f8ece253cd68282bf112fe95c6aea84863f5
unknown <damczechowski@gmail.com>
2024-12-29 19:47:38 +0100

    save new season

--- src/DataFixtures/SeasonsFixtures.php
+++ src/DataFixtures/SeasonsFixtures.php
@@ -177,6 +177,12 @@ class SeasonsFixtures extends Fixture implements OrderedFixtureInterface {
                 'dateStart' => '2024-09-02',
                 'dateEnd' => '2024-12-15',
                 'active' => true
+            ),
+            array(
+                'season_name' => 'Wiosna 2025',
+                'dateStart' => '2025-02-10',
+                'dateEnd' => '2025-05-25',
+                'active' => true
             )
         );
         
