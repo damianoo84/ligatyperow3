5ca7f601633c929b985f2a53ab3f4b865ca2aabc
unknown <damczechowski@gmail.com>
2024-12-30 13:02:25 +0100

    corrects in CronCommand.php

--- src/Command/CronCommand.php
+++ src/Command/CronCommand.php
@@ -2,23 +2,21 @@
 
 namespace App\Command;
 
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Psr\Log\LoggerInterface;
-
-use SoapClient;
+use Doctrine\ORM\EntityManagerInterface;
 
 class CronCommand extends Command {
-
-    private $container;
+  
     private $logger;
-
-    public function __construct(ContainerInterface $container, LoggerInterface $logger)
+    private $entityManager;
+    
+    public function __construct(EntityManagerInterface $entityManager, LoggerInterface $logger)
     {
         parent::__construct();
-        $this->container = $container;
+        $this->entityManager = $entityManager;
         $this->logger = $logger;
     }
 
@@ -30,35 +28,38 @@ class CronCommand extends Command {
 
     protected function execute(InputInterface $input, OutputInterface $output) : int
     {
-        $doctrine = $this->container->get('doctrine');
-        $em = $doctrine->getManager();
-        
-        $matchdayObject = $em->getRepository('App:Matchday')->getMatchday();
-        $this->logger->info('DC matchdayObject: ' . $matchdayObject);
-        
-        // pobranie listy numerów tel. użytkowników, którzy jeszcze nie wytypowali 
-        $usersPhones = $em->getRepository('App:Type')->getNoTypedUsersList($matchdayObject['name']);
+        try {
+            $matchdayObject = $this->entityManager->getRepository('App:Matchday')->getMatchday();
+            $this->logger->info('DC matchdayObject: ' . $matchdayObject);
 
-        $this->logger->info('DC $usersPhones: ' . $usersPhones);
+            // pobranie listy numerów tel. użytkowników, którzy jeszcze nie wytypowali 
+            $usersPhones = $this->entityManager->getRepository('App:Type')->getNoTypedUsersList($matchdayObject['name']);
+            $this->logger->info('DC $usersPhones: ' . $usersPhones);
 
-        ini_set("soap.wsdl_cache_enabled", "0");
-        $client = new \SoapClient("http://api.gsmservice.pl/soap/v2/gateway.php?wsdl");
-        $arAccount = array("login" => "damcio","pass" => "gutek246");
-        $arMessages = array(array(
-            "recipients" => $usersPhones,
-            "message" => "Przypomnienie o typowaniu",
-//            "message" => "To jest test. Można zignorować lub potwierdzić że doszło. Można też dopisać: nie lubię Realu i CR7 :) ",
-            "sender"=> "Damian",
-            "msgType" => 1,
-            "unicode" => false,
-            "sandbox" => false
-        ));
+            ini_set("soap.wsdl_cache_enabled", "0");
+            $client = new \SoapClient("http://api.gsmservice.pl/soap/v2/gateway.php?wsdl");
+            $arAccount = array("login" => "damcio","pass" => "gutek246");
+            $arMessages = array(array(
+                "recipients" => $usersPhones,
+                "message" => "Przypomnienie o typowaniu",
+    //            "message" => "To jest test. Można zignorować lub potwierdzić że doszło. Można też dopisać: nie lubię Realu i CR7 :) ",
+                "sender"=> "Damian",
+                "msgType" => 1,
+                "unicode" => false,
+                "sandbox" => false
+            ));
 
-        $this->logger->info('DC before send sms message');
+            $this->logger->info('DC before send sms message');
 
-        // wysłanie smsów o ustalonym w CRON terminie
-        $client->SendSMS(array("account" => $arAccount,"messages"=> $arMessages))->return;
+            // wysłanie smsów o ustalonym w CRON terminie
+            $client->SendSMS(array("account" => $arAccount,"messages"=> $arMessages))->return;
 
-        $this->logger->info('DC after send sms message');
+            $this->logger->info('DC after send sms message');
+
+            return Command::SUCCESS; // Zwróć sukces
+        } catch (\Exception $e) {
+            $this->logger->error('Error occurred: ' . $e->getMessage());
+            return Command::FAILURE; // Zwróć błąd w przypadku wyjątku
+        }
     }    
 }
