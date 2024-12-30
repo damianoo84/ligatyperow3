9c0bed78b6b5137beec4de81d8be247b8bf7b51f
unknown <damczechowski@gmail.com>
2024-12-30 23:12:06 +0100

    corrects in CronCommand

--- src/Repository/MatchdayRepository.php
+++ src/Repository/MatchdayRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Matchday;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Psr\Log\LoggerInterface;
 
 /**
  * @extends ServiceEntityRepository<Matchday>
